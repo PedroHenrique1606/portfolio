2d26dca1c84a868e3d0608a71cd76cd18e2efca8
“PedroHenrique1606” <pedromelosilva11911@gmail.com>
2023-03-13 11:37:43 -0300

    creation of the skills area, contact and addition of projects in the portfolio 13032023

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.addEventListener("scroll", function(){
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 });
+
+//PORTFOLIO
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick){
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) =>{
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) =>{
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) =>{
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
